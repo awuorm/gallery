e4e3bedcc407208c12966bbbe462fcb7b348c390
awuorm <otieno.awuor.m@gmail.com>
2023-03-01 12:13:58 +0300

    tests passing

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://mildredgallery:99DJSuEkyg28HpK@cluster0.gh90g95.mongodb.net/test',
+    development: 'mongodb+srv://mildredgallery:99DJSuEkyg28HpK@cluster0.gh90g95.mongodb.net/test',
+    test: 'mongodb+srv://mildredgallery:99DJSuEkyg28HpK@cluster0.gh90g95.mongodb.net/test',
 }
 module.exports = config;
